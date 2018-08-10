2c9d14d312a8dead36e157d71dcced864db6140a
RobertPinnixIII <roberteleepinnixiii@gmail.com>
2018-08-10 01:04:54 -0600

    figured out how to generate the random numbers for the target number and crystals

---
+++ assets/javascript/game.js
@@ -0,0 +1,38 @@
+var targetNumber = [];
+for (var i =0, t=1; i<t; i++) {
+    targetNumber.push(Math.round(Math.random() * (120 - 19) + 19));
+
+}
+console.log(targetNumber);
+
+var counter = 0;
+
+var numberOptions = [];
+for (var i=0, t=4; i<t; i++) {
+    numberOptions.push(Math.round(Math.random() * 12) + 0);
+}
+$(".targetNumber").html( " " );
+console.log(numberOptions);
+
+
+
+
+
+
+$(document).ready(function(){ 
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
