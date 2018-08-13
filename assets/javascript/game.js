a067ba26ec966df186079838de6d77f4a30a5865
RobertPinnixIII <roberteleepinnixiii@gmail.com>
2018-08-13 00:06:45 -0600

    turning in unfinished product late.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,4 @@
-$(document).ready(function(){
+
 
 
 
@@ -12,16 +12,63 @@ console.log(targetNumber);
 
 var counter = 0;
 
-var numberOptions = [];
- for (var i =0, t=1; i<t; i++) {
-    numberOptions.push(Math.round(Math.random() * (12 - 1) + 1));
-    console.log(numberOptions);
-}
 
 
 
 
-var imageCrystal = ("<img>");
+
+   
+   var crystalarray1 = (Math.round(Math.random() * (12 - 1) + 1))
+   ;
+   
+   
+   
+   console.log(crystalarray1);
+
+    
+    
+    var crystalarray2 =   (Math.round(Math.random() * (12 - 1) + 1));
+    
+   
+    console.log(crystalarray2);
+
+    var crystalarray3 = (Math.round(Math.random() * (12 - 1) + 1));
+    
+    
+    console.log(crystalarray3);
+
+    var crystalarray4 = (Math.round(Math.random() * (12 - 1) + 1));
+    
+
+    console.log(crystalarray4);
+
+
+
+
+
+    var totalscore = (crystalarray1)  + (crystalarray2)  + (crystalarray3)  + (crystalarray4);
+    
+      
+    
+    
+
+
+console.log("totalscore");
+
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
 
 
 
@@ -29,10 +76,48 @@ var imageCrystal = ("<img>");
 
 
 //jquery here
- 
+$(document).ready(function(){
 
     $(".number").html("<h2>" + targetNumber + "</h2>");
     $(".number").addClass("number");
+
+    $(".Crystal1").click(function(){
+    $(".scorebox").append(   totalscore  );
+    
+    
+    console.log("crystal1")
+
+    });
+
+    $(".Crystal2").click(function(){
+        $(".scorebox").append( totalscore);
+        console.log("crystal2")
+    });
+
+
+    $(".Crystal3").click(function(){
+        $(".scorebox").append(totalscore);
+        console.log("crystal3")
+    });
+    
+    $(".Crystal4").click(function(){
+        $(".scorebox").append(totalscore);
+        console.log("crystal4")
+
+        
+
+        
+    });
+
+
+    /*need to figure out how you get all the clcks added up as
+    one number. this is the part that giving me the hardest time.
+    the rest i know hw to write.  need to output wins and losses, and 
+    set a reset function for game over win or lose. i know how to do all of
+    that exept getting the clicks to add up. its extremely frustrating.*/ 
+
+    
+
 
 
     
